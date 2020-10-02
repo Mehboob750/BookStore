c6f67a378c97c7c18349d4e2f621475161a5623d
Mehboob750 <ms568203@gmail.com>
2020-10-02 16:57:22 +0530

    Welcome To Book Store Application

---
+++ BookStoreApplication/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Interface;
+using CommanLayer.Exception;
+using CommanLayer.Model;
+using CommanLayer.RequestModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private IUserBL userBuiseness;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserController"/> class.
+        /// </summary>
+        /// <param name="userBusiness">It is an object of IUser Business</param>
+        public UserController(IUserBL userBuiseness)
+        {
+            this.userBuiseness = userBuiseness;
+        }
+
+        /// <summary>
+        /// This Method is used for User Registration
+        /// </summary>
+        /// <param name="registerationModel">It is an object of User Model</param>
+        /// <returns>Returns the result in SMD format</returns>
+        [HttpPost]
+        [Route("Registration")]
+        [AllowAnonymous]
+        public IActionResult UserRegistration(RegisterationModel registerationModel)
+        {
+            try
+            {
+                if (registerationModel.FirstName == null || registerationModel.LastName == null || registerationModel.Role == null || registerationModel.EmailId == null || registerationModel.PhoneNumber == null || registerationModel.Gender == null || registerationModel.Password == null)
+                {
+                    throw new BookStoreException(BookStoreException.ExceptionType.NULL_FIELD_EXCEPTION, "Null Variable Field");
+                }
+                else if (registerationModel.FirstName == "" || registerationModel.LastName == "" || registerationModel.Role == "" || registerationModel.EmailId == "" || registerationModel.PhoneNumber == "" || registerationModel.Gender == "" || registerationModel.Password == "")
+                {
+                    throw new BookStoreException(BookStoreException.ExceptionType.EMPTY_FIELD_EXCEPTION, "Empty Variable Field");
+                }
+
+                // Call the User Registration Method of User Business classs
+                var response = this.userBuiseness.UserRegistration(registerationModel);
+
+                // check if Id is not equal to zero
+                if (!response.EmailId.Equals(null))
+                {
+                    bool status = true;
+                    var message = "User Registered Successfully";
+                    return this.Ok(new { status, message, data = response });
+                }
+                else
+                {
+                    bool status = false;
+                    var message = "User already Present";
+                    return this.Conflict(new { status, message });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { status = false, message = e.Message });
+            }
+        }
+    }
+}
---
+++ BookStoreApplication/Startup.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Startup.cs" company="BridgeLabz Solution">
+//  Copyright (c) BridgeLabz Solution. All rights reserved.
+// </copyright>
+// <author>Mehboob Shaikh</author>
+//-----------------------------------------------------------------------
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:FileHeaderFileNameDocumentationMustMatchTypeName", Justification = "Reviewed.")]
+
+namespace BookStoreApplication
+{
+    using BusinessLayer.Interface;
+    using BusinessLayer.Services;
+    using CommanLayer;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using RepositoryLayer;
+    using RepositoryLayer.Interface;
+    using RepositoryLayer.Services;
+    using Swashbuckle.AspNetCore.Swagger;
+
+    /// <summary>
+    /// This is an startup class
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">It contains the configuration</param>
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the Configuration
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">It contains the services</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<ApplicationDbContext>(opts => opts.UseSqlServer(this.Configuration["ConnectionStrings:BookStore"]));
+
+            // Add application service for User Business Layer
+            services.AddTransient<IUserBL, UserBL>();
+
+            // Add application service for User Repository Layer
+            services.AddTransient<IUserRL, UserRL>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Quantity Measurement API", Description = "Swagger Quantity Measurement API" });
+            });
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">It is app parameter</param>
+        /// <param name="env">It is an environment parameter</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Core API"); });
+        }
+    }
+}
